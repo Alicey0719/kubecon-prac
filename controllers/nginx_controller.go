655bd44ec6c03d0d88aa4d963baf5c0537cab31e
Alicey <sirius@alicey.dev>
2022-02-26 02:11:49 +0900

    fix import record

--- controllers/nginx_controller.go
+++ controllers/nginx_controller.go
@@ -20,6 +20,7 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	"k8s.io/client-go/tools/record"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
